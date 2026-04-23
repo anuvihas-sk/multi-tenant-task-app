19a0840e4e7229e09aa76276977cd87996dd8bc2
anuvihas-sk <anuvihas23cb005@jit.ac.in>
2026-04-24 03:43:47 +0530

    updated docker

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,9 +2,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  host: "localhost",
-  database: "taskdb",
+  host: "db", // ✅ Docker service name
   password: "password",
+  database: "taskdb",
   port: 5432,
 });
 
